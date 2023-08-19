df1468b75177baf8362efa6abb4fa906a2e7294a
Chii Yuen <chii.yuen@hotmail.com>
2023-08-19 21:46:24 +0800

    Add Dice class

---
+++ src/Dice.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Dice {
+	private static int randomNum;
+
+	// Generates and return a random number between 1 to 6
+	public static int roll() {
+		randomNum = new Random().nextInt(6) + 1;
+		return randomNum;
+	}
+}
