b501ac0fe6cf276b803c5e1e1604a790f9894740
Chii Yuen <chii.yuen@hotmail.com>
2023-08-20 02:34:34 +0800

    Reformat Scoreboard class, add classpath, project files

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.Comparator;
 
 public class Scoreboard {
 	private ArrayList<Player> topScores = new ArrayList<Player>();
@@ -8,58 +9,33 @@ public class Scoreboard {
 	private Formatter output;
 	private String filepath = "src/score.txt";
 
+	public Scoreboard() {
+		this.loadScores();
+		this.sortTopScores();
+	}
+
 	public void loadScores() {
-		input = openInputFile(filepath);
-		readScores();
-		closeInputFile(input);
+		try {
+			input = new Scanner(new File(filepath));
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found.");
+			System.exit(1);
+		}
+		while (input.hasNext()) {
+			Player player = new Player();
+			player.setName(input.next());
+			player.setScore(input.nextInt());
+			topScores.add(player);
+		}
+		closeInput(input);
 	}
 
 	public void saveScores() {
-		output = openOutputFile(filepath);
-		writeScores();
-		closeOutputFile(output);
-	}
-
-	public Scanner openInputFile(String filename) {
-		Scanner tempinput = null;
 		try {
-			tempinput = new Scanner(new File(filename));
-		} catch (FileNotFoundException fileNotFoundException) {
-			System.err.println(fileNotFoundException);
+			output = new Formatter(new File(filepath));
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found.");
 			System.exit(1);
-		}
-		return tempinput;
-	}
-
-	public Formatter openOutputFile(String filename) {
-		Formatter tempoutput = null;
-		try {
-			tempoutput = new Formatter(filename);
-		} catch (FileNotFoundException fileNotFoundException) {
-			System.err.println(fileNotFoundException);
-			System.exit(1);
-		}
-		return tempoutput;
-	}
-
-	public void closeInputFile(Scanner input) {
-		if (input != null)
-			input.close();
-	}
-
-	public void closeOutputFile(Formatter output) {
-		if (output != null)
-			output.close();
-	}
-
-	public void readScores() {
-		try {
-			while (input.hasNext()) {
-				Player player = new Player();
-				player.setName(input.next());
-				player.setScore(input.nextInt());
-				topScores.add(player);
-			}
 		} catch (NoSuchElementException elementException) {
 			System.err.println("File improperly formed.");
 			input.close();
@@ -68,34 +44,55 @@ public class Scoreboard {
 			System.err.println("Error reading from file.");
 			System.exit(1);
 		}
+		for (int j = 0; j < 5; j++) {
+			output.format("%s %d\n", topScores.get(j).getName(), topScores.get(j).getScore());
+		}
+		closeOutput(output);
 	}
 
-	public void writeScores() {
-		for (int k = 0; k < topScores.size(); k++) {
-			output.format("%s %d\n", topScores.get(k).getName(), topScores.get(k).getScore());
+	public void updateTopScores(Player newPlayer) {
+		for (int i = 0; i < topScores.size(); i++) {
+			if (newPlayer.getScore() < topScores.get(i).getScore()) {
+				topScores.add(i, newPlayer);
+				trimTopScores();
+				break;
+			}
 		}
 	}
 
-	public void addPlayer(Player a) {
-		topScores.add(a);
+	public void sortTopScores() {
+		Collections.sort(topScores, Comparator.comparing(Player::getScore));
+		trimTopScores();
 	}
 
-	public void sortTopScore() {
-		Collections.sort(topScores, new Comparator<Player>() {
-			@Override
-			public int compare(Player o1, Player o2) {
-				return Integer.valueOf(o2.getScore()).compareTo(o1.getScore());
-			}
-		});
-		Collections.reverse(topScores);
+	public void trimTopScores() {
+		while (topScores.size() > 5) {
+			topScores.remove(5);
+		}
+	}
+
+	public void closeInput(Scanner input) {
+		if (input != null)
+			input.close();
+	}
+
+	public void closeOutput(Formatter output) {
+		if (output != null) {
+			output.close();
+		}
+	}
+
+	public ArrayList<Player> getTopScores() {
+		return topScores;
 	}
 
 	public String toString() {
-		String board = "Score Board: \n";
-		for (int i = 0; i < 5; i++) {
-			board += String.format("%d. %s %d rounds\n", i + 1, topScores.get(i).getName(),
-					topScores.get(i).getScore());
+		String scoreString = "\nHigh Scores (Moves Taken)\n\n";
+		int counter = 1;
+		for (Player topScorer : topScores) {
+			scoreString += counter + ". ----- " + topScorer.getName() + " ----- " + topScorer.getScore() + "\n";
+			counter++;
 		}
-		return board;
+		return scoreString;
 	}
 }
